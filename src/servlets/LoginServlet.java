5f74ae14544320bd82e51601a38d5dba1ca9f8b9
junpeng1989 <junpeng.li_1110@yahoo.com>
2021-09-18 10:51:35 +0800

    update all

---
+++ src/servlets/LoginServlet.java
@@ -0,0 +1,56 @@
+package servlets;
+
+import java.io.*;
+import java.io.IOException;
+import java.net.http.HttpResponse;
+import java.sql.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/Login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//HTTPSession
+		HttpSession session = request.getSession();
+		
+		System.out.println("comnes in");
+		//HttpResponse
+        response.setContentType("text/html");
+        response.sendRedirect("Login.jsp");
+        
+	
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
